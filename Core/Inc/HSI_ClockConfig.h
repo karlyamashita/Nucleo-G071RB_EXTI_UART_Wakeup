1ac9f92468c39e873d6b58689094b9989c6bba5e
Karl Yamashita <karl.yamashita@advantest.com>
2025-07-18 14:37:43 -0700

    Added HSI Clock config file

---
+++ Core/Inc/HSI_ClockConfig.h
@@ -0,0 +1,14 @@
+/*
+ * LSI_ClockConfig.h
+ *
+ *  Created on: Jul 16, 2025
+ *      Author: karl.yamashita
+ */
+
+#ifndef INC_HSI_CLOCKCONFIG_H_
+#define INC_HSI_CLOCKCONFIG_H_
+
+void HSI_SystemClock_Config(void);
+
+
+#endif /* INC_HSI_CLOCKCONFIG_H_ */
